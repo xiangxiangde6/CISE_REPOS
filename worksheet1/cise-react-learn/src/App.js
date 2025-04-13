90ebc188645aecc6929ae60d6b90f263b5868f90
xiangxiangde6 <liuyixiang340@gmail.com>
2025-04-13 21:58:36 +1200

    Update App.js link to AUT site in LogoLink branch

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -11,10 +11,11 @@ function App() {
         </p>
         <a
         className="App-link"
-        href="https://reactjs.org"
+        href="https://aut.ac.nz"
         target="_blank"
         rel="noopener noreferrer"
 >
+
      Agile is about Values and Principles
 
 </a>
