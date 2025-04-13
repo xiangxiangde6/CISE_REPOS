88d7f720b79dc75ef07053093e7472d0a0ac9b14
xiangxiangde6 <liuyixiang340@gmail.com>
2025-04-13 21:30:03 +1200

    Update App.js: replace 'Learn React' with Agile message

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -10,13 +10,15 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+>
+     Agile is about Values and Principles
+
+</a>
+
       </header>
     </div>
   );
